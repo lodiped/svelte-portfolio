5a4c752ae2bade8160b4a6d502e4ba8c981f5d43
lodiped <lodiped@gmail.com>
2024-03-21 12:37:03 -0300

    add Threlte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>lodiped</h1>
-<p>Graphic Designer turned Web Developer.</p>
+<main class="">
+	<div class="flex justify-between p-5">
+		<a href="/">lodiped</a>
+		<div class="flex gap-5">
+			<a href="/">about me</a>
+			<a href="/">projects</a>
+			<a href="/">minigames</a>
+		</div>
+	</div>
+	<div class="p-10">
+		<div class="bg-green-500 aspect-square m-20">Some 3D</div>
+		<p>Graphic Designer turned Web Developer.</p>
+		<p>Graphic Designer turned Web Developer.</p>
+	</div>
+</main>
